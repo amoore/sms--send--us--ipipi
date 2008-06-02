405f3421f8cdf20ad64df3aa4d3385f5adbadcc3
Andrew Moore <andrew.moore@liblime.com>
2008-06-02 10:08:33 -0500

    incrementing version

--- lib/SMS/Send/US/Ipipi.pm
+++ lib/SMS/Send/US/Ipipi.pm
@@ -8,11 +8,11 @@ SMS::Send::US::Ipipi - An SMS::Send driver for the ipipi.com website
 
 =head1 VERSION
 
-Version 0.02
+Version 0.03
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 =head1 SYNOPSIS
 
